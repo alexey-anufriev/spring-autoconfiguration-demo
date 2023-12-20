c76ab2672dba8e8ad87e6d3c4a8066c38bb35f81
Alexey Anufriev <a.anufriev@celonis.de>
2023-12-20 11:17:36 +0100

    test: cover main problem

--- src/test/java/com/alexeyanufriev/autoconfigdemo/app/AutoconfigurationDemoApplicationTests.java
+++ src/test/java/com/alexeyanufriev/autoconfigdemo/app/AutoconfigurationDemoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.alexeyanufriev.autoconfigdemo.app;
 
+import com.alexeyanufriev.autoconfigdemo.app.beans.DependencyBean;
+import com.alexeyanufriev.autoconfigdemo.app.beans.MainBean;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AutoconfigurationDemoApplicationTests {
 
+	@Autowired(required = false)
+	private MainBean mainBean;
+
+	@Autowired(required = false)
+	private DependencyBean dependencyBean;
+
 	@Test
-	void contextLoads() {
+	void completeSetOfBeansShouldBeAutowired() {
+		Assertions.assertThat(this.mainBean).isNotNull();
+		Assertions.assertThat(this.dependencyBean).isNotNull();
 	}
 
 }
